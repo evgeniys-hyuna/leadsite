9f73afe1a85e32ca22604e218c5684a7576899c6
siderka.eugene <siderka.eugene@gmail.com>
2015-11-30 17:23:41 +0200

    keyword pages updated

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,18 +15,22 @@
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
 
+        <!--JQuery-->
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        
         <!--Bootstrap-->
         <!-- Latest compiled and minified CSS -->
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
         <!-- Optional theme -->
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
         <!-- Latest compiled and minified JavaScript -->
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
         
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     </head>
 
-    <body>
+    <body style="background-color: #EEE;">
 
         <div class="container" id="page">
 
--- protected/views/site/keyword_alexa.php
+++ protected/views/site/keyword_alexa.php
@@ -2,58 +2,77 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Keyword ALEXA</h1>
-<h3><?= $keyword->name ?></h3>
+<div class="col-md-4">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Keyword ALEXA</h3>
+        </div>
+        <div class="panel-body">
+            <h3><?= $keyword->name ?></h3>
+            
+            <div>
+                <p>
+                    Alexa search method:
 
-<p><?= CHtml::link('<< Back', Yii::app()->createUrl('site/keywordDetails', array(
-    'keywordId' => $keyword->id,
-))) ?></p>
+                    <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_FULL ? '<b>Full</b>' : CHtml::link('Full', Yii::app()->createUrl('site/keywordAlexa', array(
+                        'keywordId' => $keyword->id,
+                        'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_FULL,
+                    ))) ?>
 
-<div>
-    <p>
-        Alexa search method:
-    
-        <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_FULL ? '<b>Full</b>' : CHtml::link('Full', Yii::app()->createUrl('site/keywordAlexa', array(
-            'keywordId' => $keyword->id,
-            'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_FULL,
-        ))) ?>
+                    <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_COMBO ? '<b>Combo</b>' : CHtml::link('Combo', Yii::app()->createUrl('site/keywordAlexa', array(
+                        'keywordId' => $keyword->id,
+                        'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_COMBO,
+                    ))) ?>
 
-        <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_COMBO ? '<b>Combo</b>' : CHtml::link('Combo', Yii::app()->createUrl('site/keywordAlexa', array(
-            'keywordId' => $keyword->id,
-            'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_COMBO,
-        ))) ?>
-    
-        <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_PARTIAL ? '<b>Partial</b>' : CHtml::link('Partial', Yii::app()->createUrl('site/keywordAlexa', array(
-            'keywordId' => $keyword->id,
-            'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_PARTIAL,
-        ))) ?>
-    </p>
-    <p><i>
-        Full: searches all words from keyword in domain. Like: "watch movie" will be founded in "watchonlinemovie", but not in "watchonline". <br />
-        Combo: searches all combinations of all words from keyword. Like: "watch movie" will be founded in "moviewatch", but not in "watchonlinemovie" <br />
-        Partial: searches at least for 1 of words from keyword. Like: "watch movie" will be founded in "watchonline". <br />
-    </i></p>
+                    <?= $alexaSearchMethod == Keyword::ALEXA_SEARCH_METHOD_PARTIAL ? '<b>Partial</b>' : CHtml::link('Partial', Yii::app()->createUrl('site/keywordAlexa', array(
+                        'keywordId' => $keyword->id,
+                        'alexaSearchMethod' => Keyword::ALEXA_SEARCH_METHOD_PARTIAL,
+                    ))) ?>
+                </p>
+                <p><i>
+                    <p>Full: searches all words from keyword in domain. Like: "watch movie" will be founded in "watchonlinemovie", but not in "watchonline".</p>
+                    <p>Combo: searches all combinations of all words from keyword. Like: "watch movie" will be founded in "moviewatch", but not in "watchonlinemovie"</p>
+                    <p>Partial: searches at least for 1 of words from keyword. Like: "watch movie" will be founded in "watchonline".</p>
+                </i></p>
+            </div>
+
+            <p><?= CHtml::link('<< Back', Yii::app()->createUrl('site/keywordDetails', array(
+                'keywordId' => $keyword->id,
+            )), array(
+                'class' => 'btn btn-info',
+            )) ?></p>
+        </div>
+    </div>
 </div>
 
-<div>
-    <?php $this->widget('zii.widgets.grid.CGridView', array(
-        'id' => 'grvAlexa',
-        'dataProvider' => $keyword->searchAlexaRankings($alexaSearchMethod),
-        'columns' => array(
-            array(
-                'name' => 'position',
-                'header' => 'Position',
-                'value' => function ($e) {
-                    return $e['position'];
-                }
-            ),
-            array(
-                'name' => 'domain',
-                'header' => 'Domain',
-                'value' => function ($e) {
-                    return $e['domain'];
-                }
-            ),
-        ),
-    )) ?>
+<div class="col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Search Results</h3>
+        </div>
+        <div class="panel-body">
+            <div>
+                <?php $this->widget('zii.widgets.grid.CGridView', array(
+                    'id' => 'grvAlexa',
+                    'dataProvider' => $keyword->searchAlexaRankings($alexaSearchMethod),
+                    'columns' => array(
+                        array(
+                            'name' => 'position',
+                            'header' => 'Position',
+                            'value' => function ($e) {
+                                return $e['position'];
+                            }
+                        ),
+                        array(
+                            'name' => 'domain',
+                            'header' => 'Domain',
+                            'value' => function ($e) {
+                                return $e['domain'];
+                            }
+                        ),
+                    ),
+                )) ?>
+            </div>
+        </div>
+    </div>
 </div>
